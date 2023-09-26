c7e015360781cede73b683a6e9f7af8c61d7e2f9
alessandrostorchi92 <storchi.alle@gmail.com>
2023-09-26 19:03:24 +0200

    creation db adn import movies table

---
+++ resources/layouts/public.blade.php
@@ -0,0 +1,26 @@
+{{-- Questo file deve contenere tuti gli elementi visibili agli utenti che si ripetono in tutte le pagine del sito web  --}}
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Oggi facciamo la nostra prima vera interazione con il database utilizzando l'ORM di Laravel.">
+
+    <title> Laravel-Model-Controller</title>
+
+    {{-- Questa riga di codice serve per caricare il scss e js --}}
+    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+
+</head>
+
+<body>
+
+
+</body>
+
+</html>
